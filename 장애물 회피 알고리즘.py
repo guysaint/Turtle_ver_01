83e7da20ee82d1b6f77b0e2dd6126e5c3e478991
guysaint <guysaint@naver.com>
2025-07-22 14:23:21 +0900

    장애물 회피 알고리즘 작성
    장애물 충돌시 회피하는 방법을 랜덤으로 각도 변경, 랜덤으로 전진하는 코드를 추가

--- 장애물 회피 알고리즘.py
+++ 장애물 회피 알고리즘.py
@@ -35,15 +35,16 @@ t.end_fill()
 t.color("skyblue")
 t.penup()
 t.goto(-300,-270)
+t.pendown()
 
 # 시작점에서 도착점으로 장애물 피해서 이동하기
-t.pendown()
-t.goto(-40, -30)
-t.lt(90)
-t.fd(20)
-t.rt(30)
-t.fd(50)
-t.goto(325,275)
+# t.pendown()
+# t.goto(-40, -30)
+# t.lt(90)
+# t.fd(20)
+# t.rt(30)
+# t.fd(50)
+#t.goto(325,275)
 
 # 거리를 계산하는 함수
 def cal_distance(x1, y1, x2, y2):
@@ -52,8 +53,6 @@ def cal_distance(x1, y1, x2, y2):
     return distance
     
 print(f"시작점과 도착점까지의 거리: {cal_distance(-300,-270,325,275)}")
-
-
 
 
 #거북이의 반지름 크기 변수 설정
@@ -73,6 +72,29 @@ def check_collision():
     #충돌 판정: 거북이 크기(0.75) + 장애물 반지름(25)
     if col_distance <= tradius+25:
         return True #충돌
-return False    #안전
+    return False    #안전
     
+def avoid_obstacle():
+    #장애물 회피 행동
+    print("장애물 감지! 회피 시작")
+    
+    #1단계: 랜덤한 회전 각도 생성(30~150도)
+    turn_angle = random.randint(30,150)
+    print(f"회전 각도:{turn_angle}도")
+    
+    #2단계
+    direction = random.choice([1,-1]) #1=좌회전, -1=우회전
+    
+    if direction ==1:
+        t.lt(turn_angle)
+        print(f"좌회전 {turn_angle}도")
+        
+    else:
+        t.rt(turn_angle)
+        print(f"우회전 {turn_angle}도")
+        
+    #3단계: 회전 후 안전 거리만큼 이동
+    move_distance = random.randint(20,50)
+    t.fd(move_distance)
+    print(f"{move_datance}픽셀 이동 완료")
     
